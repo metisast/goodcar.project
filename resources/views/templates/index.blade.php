f6e1bca37fc315accf157a659ba8e29b3de79c1b
eugene <metis_ast@mail.ru>
2015-09-09 15:47:58 +0600

    Include GoogleFonts: Open Sans

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Goodcar</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="styles/reset.css"/>
 </head>
 <body>
-    
+    <div id="container">
+        <div id="top-links">
+
+        </div>
+    </div>
 </body>
 </html>
